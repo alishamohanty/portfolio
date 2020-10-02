46176884ea73c89437ade2491d8c9268f244f435
Alisha Mohanty <alishamohanty1416@gmail.com>
2020-10-02 21:50:42 +0530

    Refine Code

---
+++ layouts/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <footer class="footer-distributed">
+        <div class="footer-left">
+            <p class="footer-company-about">
+            <span>Feel free to say Hi! <a href="#" class="footer-icons"><font-awesome-icon :icon="['fas', 'envelope']"/></a></span>
+            You are magnificient beyond measure, perfect in your own imperfections and wonderfully made.</p>
+        </div>
+        <div class="footer-right">  
+            <div class="footer-icons">
+                <a href="#"><font-awesome-icon :icon="['fab', 'facebook']"/></a>
+                <a href="#"><font-awesome-icon :icon="['fab', 'medium']"/></a>
+                <a href="#"><font-awesome-icon :icon="['fab', 'twitter']"/></a>
+                <a href="#"><font-awesome-icon :icon="['fab', 'linkedin']"/></a>
+                <a href="#"><font-awesome-icon :icon="['fab', 'github']"/></a>
+            </div>
+            <p class="footer-company-name">Copyright &copy {{ new Date().getFullYear() }} Alisha Mohanty</p>
+        </div>
+    </footer>
+</template>
---
+++ layouts/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="header">
+    <div class="logo">
+      <img/>
+    </div>
+    <div class="side_options">
+      <ul>
+        <li>Projects</li>
+        <li>Experience</li>
+        <li>About Me</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .header{
+      display: flex;
+      justify-content: space-between;
+  }
+
+  .side_options ul li{
+    list-style: none;
+    text-align: center;
+    display: inline;
+    margin: 0 2rem;
+    cursor: pointer;
+  }
+
+  .logo{
+    width: 130px;
+    height: 50px;
+    background-color: blue;
+  }
+
+</style>
